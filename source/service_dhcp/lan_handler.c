7f2c2d2d14d3abe27902aeeb16c7fe74e93bf85e
Andre McCurdy <armccurdy@gmail.com>
2020-04-06 19:19:20 +0000

    autotools cleanup
     - Drop hardcoded references to /var/tmp/pc-rdkb from include paths
       etc. There should be no absolute include paths in the source code.
     - Add -lrt to AM_LDFLAGS when building the firewall binary. The
       firewall.c source contains references to shm_open(), so should be
       linked with -lrt.
     - Fix quoting for PLATFORM_HAL_SYNDICATION test. Avoid warnings when
       running configure:
    
        ./configure: line 12402: test: =: unary operator expected
    
     - Sort and reformat AC_CONFIG_FILES in configure.ac
     - Drop unnecessary attempts to link with zlib
     - Fix cJSON.h include path ( should be #include <cjson/cJSON.h> )
       ie Don't rely on RDK specific workaround of creating a cJSON.h
       symlink in the top level include directory.
     - Add -lcimplog to LDFLAGS as required + fix cimplog.h include path
     - Drop unnecessary $(top_srcdir)/source include paths
     - Drop global -lpthread
     - Don't add dirs above ${top_srcdir} to include paths
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/service_dhcp/lan_handler.c
+++ source/service_dhcp/lan_handler.c
@@ -29,7 +29,7 @@
 #include "print_uptime.h"
 #include <telemetry_busmessage_sender.h>
 #ifdef FEATURE_SUPPORT_ONBOARD_LOGGING
-#include "cimplog.h"
+#include <cimplog/cimplog.h>
 #define LOGGING_MODULE "Utopia"
 #define OnboardLog(...)                 onboarding_log(LOGGING_MODULE, __VA_ARGS__)
 #else
