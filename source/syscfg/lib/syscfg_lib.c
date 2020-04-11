30daac006ebf063a1f7366368bf796c381c0b8ef
Andre McCurdy <armccurdy@gmail.com>
2020-04-10 21:45:30 -0700

    drop usage of obsolete pthread_mutexattr_setrobust_np() etc
    Replace non-portable glibc functions with the Posix versions.
    
      http://man7.org/linux/man-pages/man3/pthread_mutexattr_setrobust.3.html
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/syscfg/lib/syscfg_lib.c
+++ source/syscfg/lib/syscfg_lib.c
@@ -651,9 +651,9 @@ static int lock_init (syscfg_shm_ctx *ctx)
             err, strerror(err));
         return ERR_SEMAPHORE_INIT;
     }
-    err = pthread_mutexattr_setrobust_np(&mattr, PTHREAD_MUTEX_ROBUST_NP);
+    err = pthread_mutexattr_setrobust(&mattr, PTHREAD_MUTEX_ROBUST);
     if (err) {
-        ulog_errorf(ULOG_SYSTEM, UL_SYSCFG, "pthread_mutexattr_setrobust_np error %d: %s\n",
+        ulog_errorf(ULOG_SYSTEM, UL_SYSCFG, "pthread_mutexattr_setrobust error %d: %s\n",
             err, strerror(err));
         return ERR_SEMAPHORE_INIT;
     }
@@ -693,7 +693,7 @@ static inline int read_lock (syscfg_shm_ctx *ctx)
                 fclose(consolefp);
         }
        ulog_errorf(ULOG_SYSTEM, UL_SYSCFG, "Process %d got EOWNERDEAD for read mutex\n", (int) getpid());
-        err = pthread_mutex_consistent_np(&ctx->cb.read_lock);
+        err = pthread_mutex_consistent(&ctx->cb.read_lock);
         //ulog(ULOG_SYSTEM, UL_SYSCFG, "Process %d locked read mutex\n", (int) getpid());
 
     }
@@ -719,7 +719,7 @@ static inline int write_lock (syscfg_shm_ctx *ctx)
                 fclose(consolefp);
         }
        ulog_errorf(ULOG_SYSTEM, UL_SYSCFG, "Process %d got EOWNERDEAD for write mutex\n", (int) getpid());
-       err = pthread_mutex_consistent_np(&ctx->cb.write_lock);
+       err = pthread_mutex_consistent(&ctx->cb.write_lock);
         //ulog(ULOG_SYSTEM, UL_SYSCFG, "Process %d locked write mutex\n", (int) getpid());
     }
 
@@ -766,7 +766,7 @@ static inline int commit_lock (syscfg_shm_ctx *ctx)
                 fclose(consolefp);
         }
         ulog_errorf(ULOG_SYSTEM, UL_SYSCFG,"Process %d got EOWNERDEAD for commit mutex\n", (int) getpid());
-        err = pthread_mutex_consistent_np(&ctx->cb.commit_lock);
+        err = pthread_mutex_consistent(&ctx->cb.commit_lock);
         //ulog(ULOG_SYSTEM, UL_SYSCFG,"Process %d locked commit mutex\n", (int) getpid());
     }
 
