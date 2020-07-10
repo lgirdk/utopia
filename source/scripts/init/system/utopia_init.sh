22cbd723fa4307e18333a5e4f3610348764c289d
Andre McCurdy <armccurdy@gmail.com>
2020-07-10 16:35:45 -0700

    utopia_init.sh: source log_capture_path.sh before first call to echo_t
    Ensure that echo_t is defined before first use.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/scripts/init/system/utopia_init.sh
+++ source/scripts/init/system/utopia_init.sh
@@ -44,13 +44,14 @@ CCSPPATH="/usr/ccsp"
 
 ls /tmp/pam_initialized* > /tmp/pam_init_status
 
+source $UTOPIA_PATH/log_capture_path.sh
+
 echo_t "*******************************************************************"
 echo_t "*                                                                  "
 echo_t "* Copyright 2014 Cisco Systems, Inc.                               "
 echo_t "* Licensed under the Apache License, Version 2.0                   "
 echo_t "*******************************************************************"
 
-source $UTOPIA_PATH/log_capture_path.sh
 if [ -f /etc/device.properties ]
 then
     source /etc/device.properties
