734c141e7ceab70fd33b900df461a79b18ff85c5
Sai Charan <ssirpa.contractor@libertyglobal.com>
2020-07-30 15:29:42 +0200

    add support for DNS Relay parameters ( RDKB-4543 )
    FEATURE DESCRIPTION : Implementation of DNS Relay parameters
    
    - Removed customer index dependency as it is not defined in MNG MV1
    
    TESTING PERFORMED:
    
    1) Bring the CM online.
    2) Disable DNS proxy by disabling DNS relay.
    3) DNS requests are relayed to WAN DNS servers
    4) Enable proxy and dns requests are resolved by the brlan0.

--- source/scripts/init/service.d/set_resolv_conf.sh
+++ source/scripts/init/service.d/set_resolv_conf.sh
@@ -114,6 +114,69 @@ prepare_resolv_conf () {
 
    echo $IPv6_NAMESERVERS >> $RESOLV_CONF
 }
+# LGI ADD START
+source /etc/utopia/service.d/service_dhcp_server/dhcp_server_functions.sh
+lgi_prepare_resolv_conf () {
+    SEARCH_DOMAIN=`cat $RESOLV_CONF | grep search`
+    WAN_DNS_IPv4=`sysevent get wan_dhcp_dns`
+    WAN_DNS_IPv6=`sysevent get ipv6_nameserver`
 
-prepare_resolv_conf
+    echo -n  > $RESOLV_CONF
+
+    # Write domain.
+    echo "$SEARCH_DOMAIN" >> $RESOLV_CONF
+
+    # IPv4
+    # Write loopback address in the beginning.
+    echo "nameserver 127.0.0.1" >> $RESOLV_CONF
+
+    # Write static configuration.
+    get_static_dns_ips ipv4 wan
+    for ip in $STATIC_DNS_IPv4
+    do
+        echo "nameserver $ip" >> $RESOLV_CONF
+    done
+
+    # Write DHCP configuration.
+    if [ -z "$STATIC_DNS_IPv4" ] ; then
+        for ip in $WAN_DNS_IPv4;
+        do
+            echo "nameserver $ip" >> $RESOLV_CONF
+        done
+    fi
+
+    # IPv6
+    # Write loopback address in the beginning.
+    echo "nameserver ::1" >> $RESOLV_CONF
+
+    # Write static configuration.
+    get_static_dns_ips ipv6 wan
+    for ip in $STATIC_DNS_IPv6
+    do
+        echo "nameserver $ip" >> $RESOLV_CONF
+    done
+
+    # Write DHCP configuration.
+    if [ -z "$STATIC_DNS_IPv6" ] ; then
+        for ip in $WAN_DNS_IPv6;
+        do
+            echo "nameserver $ip" >> $RESOLV_CONF
+        done
+    fi
+
+    WAN_STATIC_DOMAIN=`syscfg get wan_domain`
+    WAN_ADDRESS_MODE=`syscfg get wan_proto`
+    if [ "" != "$WAN_STATIC_DOMAIN " ] && [ "static" == "$WAN_ADDRESS_MODE" ]; then
+        echo "search $WAN_STATIC_DOMAIN" >> $RESOLV_CONF
+    fi
+
+    # Restart DHCP server.
+    #sysevent set dhcp_server-restart (Uncomment me after fixing : RDKB-4563)
+    /etc/utopia/service.d/service_dhcp_server.sh dhcp_server-restart
+}
+# LGI ADD END
+# LGI MOD START
+#prepare_resolv_conf
+lgi_prepare_resolv_conf
+# LGI MOD END
 
