8aecc3b3eeca16e6e90dca043f8375174d463773
Sandipan Sinha <ssinha.contractor@libertyglobal.com>
2020-08-09 15:30:03 +0200

    add support for Device.X_LGI-COM_Gateway.IPv6LANMode parameters for DHCPv6 ( CR017 : RDKB-4586 )
    RDKB-4586 CR017- DNS setting for both dibbler and zebra services
    when DNS proxy feature is enabled or disabled, set the routing advertisement
    packet flags according to the IPv6LANMode selected and running
    zebra with the changed configuration when necessary.
    
    NEW FEATURE:
    CR017: New LGI-COM Data Model parameter for DHCPv6
    Stateful/Stateless configuration.
    
    FEATURE DESCRIPTION:
    
    - Added custom data model "Device.X_LGI-COM_Gateway.IPv6LANMode" for DHCPv6
      stateful/stateless configuration.
    
    - Update M-bit, O-bit, and A-bit settings in the IPv6
      Router Advertisements for Stateful and Stateless mode.
    
    - As per Observations,
      M-bit- Managed Address Conf Flag. It indicates that the client may use
             DHCPv6 to retrieve a Managed IPv6 address from a DHCPv6 server
    
      O-bit- Other Conf Flag. It indicates that other configuration information
             like DNS-related information is available via DHCPv6.
    
      A-bit- Implemented code changes for A-bit as per the requirement.
             It is for AutoConf
    
    TESTING PERFORMED:
    
    1. Make sure DUT online.
    2. Check for the new TR-181 datamodel through dmcli
       Device.X_LGI-COM_Gateway.IPv6LANMode.
    3. Switch between "Stateful" and "Stateless" modes
    4. GPV/SPV of parameters via dmcli and tr069.
    5. Made sure connected lan client gets IPv6 address for both Stateful and Stateless modes.
    6. Verified flag values of router advertisement with the help of packet capture via wireshark.
    
    Signed-off-by: Sandipan Sinha <ssinha.contractor@libertyglobal.com>

---
+++ source/scripts/init/service.d/set_ipv6_dns.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+#######################################################################
+# Copyright 2018-2019 ARRIS Enterprises, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#######################################################################
+source /etc/utopia/service.d/service_dhcp_server/dhcp_server_functions.sh
+
+dibbler_conf=/etc/dibbler/server.conf
+zebra_conf=/var/zebra.conf
+tool=$1
+ips=""
+dns_proxy=`syscfg get dns_relay_enable`
+dhcp_dns_ips=`sysevent get ipv6_nameserver`
+DNS_OVERRIDE=`syscfg get dns_override`
+
+if [ "$DNS_OVERRIDE" == "true" ] ; then
+    DNS_IPv6_PREFERRED=`syscfg get dns_ipv6_preferred`
+    DNS_IPv6_ALTERNATE=`syscfg get dns_ipv6_alternate`
+    if [ -n "$DNS_IPv6_PREFERRED" ] ; then
+        ips=$DNS_IPv6_PREFERRED
+    fi
+    if [ -n "$DNS_IPv6_ALTERNATE" ] ; then
+        ips=$ips" "$DNS_IPv6_ALTERNATE
+    fi
+else
+    if [ "$dns_proxy" == "1" ] ; then
+        ips=`sysevent get lan_ipaddr_v6`
+    else
+        # Get LAN static configuration.
+        get_static_dns_ips ipv6 lan
+        if [ -z "$STATIC_DNS_IPv6" ] ; then
+            # Get WAN static configuration.
+            get_static_dns_ips ipv6 wan
+        fi
+
+        if [ -z "$STATIC_DNS_IPv6" ] ; then
+            # Use dynamic configuration.
+            ips=$dhcp_dns_ips
+        else
+            # Use static configuration.
+            ips=$STATIC_DNS_IPv6
+        fi
+    fi
+fi
+
+# Trim spaces.
+shopt -s extglob
+ips="${ips%%*( )}"
+shopt -u extglob
+
+if [ "$tool" == "dibbler" ] ; then
+    ips="${ips// /,}"
+    sed -ri "s/ *dns-server.*$/ dns-server $ips/g" $dibbler_conf
+elif [ "$tool" == "zebra" ] ; then
+    ops=""
+    for ip in $ips;
+    do
+        if [ -z "$ops" ] ; then
+            ops="   ipv6 nd rdnss $ip 300"
+        else
+            ops="$ops\n   ipv6 nd rdnss $ip 300"
+        fi
+    done
+
+    sed -ri "/ipv6 nd rdnss.*$/d" $zebra_conf
+    IFS=
+    while read -r line
+    do
+        echo "$line"
+        if [[ "$line" == *"router-preference"* ]] ; then
+            echo -e "$ops"
+        fi
+    done < $zebra_conf > $zebra_conf".tmp" && mv $zebra_conf".tmp" $zebra_conf
+fi
