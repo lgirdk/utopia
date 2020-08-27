4a01aa97601fd0e2c6b86160cf8a08ccc801ccc0
anil shetty <adshetty@libertyglobal.com>
2020-08-27 08:21:34 -0700

    use C code to access Internal and external switch instead of shell script (handle_sw.sh)

--- source/service_multinet/Puma6_plat/puma6_plat_sw.c
+++ source/service_multinet/Puma6_plat/puma6_plat_sw.c
@@ -98,7 +98,7 @@ int configVlan_ESW(PSWFabHALArg args, int numArgs, BOOL up)
             ERR_CHK(rc);
         }
     }
-#if defined(_COSA_INTEL_XB3_ARM_)
+#if defined(_COSA_INTEL_XB3_ARM_) || defined(_PUMA6_ARM_)
 	if (up)
 	{
 		MNET_DEBUG("Adding External ports:%s\n" COMMA ifname)
@@ -147,7 +147,7 @@ int configVlan_WiFi(PSWFabHALArg args, int numArgs, BOOL up)
         }
     }
   
-#if defined(_COSA_INTEL_XB3_ARM_)
+#if defined(_COSA_INTEL_XB3_ARM_) || defined(_PUMA6_ARM_)
     if (up)
     {
 		MNET_DEBUG("Adding ATOM ports:%s\n" COMMA portID)
@@ -210,7 +210,7 @@ int configVlan_ISW(PSWFabHALArg args, int numArgs, BOOL up)
         portState = (PSwPortState) args[i].portID;
         stringIDIntSw(portState, ifname, sizeof(ifname));
         
-#if defined(_COSA_INTEL_XB3_ARM_)
+#if defined(_COSA_INTEL_XB3_ARM_) || defined(_PUMA6_ARM_)
 		if (up)
 		{
 			if (args[i].vidParams.tagging)
