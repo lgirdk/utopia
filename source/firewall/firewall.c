2570d3aa1914f7f39ecc478230d35c8d8d917114
Andre McCurdy <armccurdy@gmail.com>
2020-09-08 14:20:35 -0700

    firewall: fix return type for ClearEstbConnection() + make static
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/firewall/firewall.c
+++ source/firewall/firewall.c
@@ -13420,7 +13420,7 @@ BOOL validate_mac(char * physAddress)
 					
 	return FALSE;
 }
-void ClearEstbConnection()
+static int ClearEstbConnection(void)
 {
 char mac[20];
 char buf[200] = {0};
@@ -13446,6 +13446,8 @@ memset(buf,0,200);
 		  pclose(fp);
 		  system("rm /tmp/conn_mac");  
     }
+
+    return 0;
 }
 
 /*
