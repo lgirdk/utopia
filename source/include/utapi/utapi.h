945c85fb9375334d615c2d2f9495f2f2bbeefbef
BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
2020-10-12 15:14:03 +0200

    Add support for X_RDKCENTRAL_RuleSource parameter to differentiate rules from UPnP tool and GUI
    FIX DESCRIPTION:
    
      - Added new TR parameters in PortMapping table.
    
      - Used "Device.NAT.PortMapping.{i}.X_RDKCENTRAL_RuleSource" TR to
        differentiate the rules added from GUI and UPnP tool.
    
    TESTING PERFORMED:-
    
      1. Bring the modem online.
      2. Added a pormapping entry dmcli eRT addtable Device.NAT.PortMapping.
      3. Enabled port mapping dmcli eRT setv Device.NAT.X_Comcast_com_EnablePortMaping bool true
      4. Enabled port mapping rule dmcli eRT setv Device.NAT.PortMapping.1.Enable bool true
      5. Verified that the Device.NAT.PortMapping.1.X_RDKCENTRAL_RuleSource returns "Manual"

--- source/include/utapi/utapi.h
+++ source/include/utapi/utapi.h
@@ -375,7 +375,8 @@ typedef struct portFwdSingle {
     int        external_port;
     int        internal_port;
     char       dest_ip[IPADDR_SZ];              
-    char       dest_ipv6[64];              
+    char       dest_ipv6[64];
+    char       rule_source[32];              
 } portFwdSingle_t;
 
 typedef struct portMapDyn {
@@ -388,6 +389,7 @@ typedef struct portMapDyn {
     int        internal_port;
     int        lease;
     time_t     last_updated;
+    char       rule_source[32]; 
 } portMapDyn_t;
 
 typedef struct portFwdRange {
@@ -401,8 +403,9 @@ typedef struct portFwdRange {
     int        internal_port;
     int        internal_port_range_size;
     char       dest_ip[IPADDR_SZ];              
-    char       dest_ipv6[64];    
-	char       public_ip[IPADDR_SZ];              
+    char       dest_ipv6[64];   
+    char       rule_source[32]; 
+    char       public_ip[IPADDR_SZ];              
 } portFwdRange_t;
 
 typedef struct portRangeTrig {
