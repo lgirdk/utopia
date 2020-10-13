d2611ac02c4c2c8839580485f9f4a17ffc5c8ef2
sreesreekumar <ssreekumar.contractor@libertyglobal.com>
2020-10-13 23:13:31 +0200

    RDKB-4782 - Disable port forward feature will not clear the port forward rules in iptables
    BUG DESCRIPTION:
        - Add a v4 port forward rule. Disable port forward feature , not delete the existed port forward rule.
        - The filter and nat rule is not deleted, if the feature is disabled.
    
    FIX DESCRIPTION:
        - Port Forward Feature enable/disable is stored in syscfg.db as
          "CosaNAT::port_forward_enabled", with both namespace and key hardcoded.
        - Made the changes in firewall.c such that, the rules will be added only
          when the feature is enabled.
    
    TESTING PERFORMED:
    1.  Do factory default.
    2.  Add two v4 port forward rules.
    3.  Check iptables rule and see the relevant rules were added.
    4.  Disable port forward feature , not delete the existed port
        forward rules.
    5.  Verified that disabling the feature will not add any rules related
        to port forwarding which is added in step 2.
    6.  Enable the feature and disable any one of the added rules.
    7.  Verified that the iptables updated only for enabled rules.
    
    Signed-off-by: Sreeja Sreekumar(ssreekumar.contractor@libertyglobal.com)

--- source/firewall/firewall.c
+++ source/firewall/firewall.c
@@ -3365,6 +3365,10 @@ static int do_single_port_forwarding(FILE *nat_fp, FILE *filter_fp, int iptype, 
    }
 #endif
 
+   syscfg_get("CosaNAT", "port_forward_enabled", query, sizeof(query));
+   if ((query[0] != '\0') && (atoi(query) != 0))
+   {
+
    for (idx=1 ; idx<=count ; idx++) {
       namespace[0] = '\0';
       snprintf(query, sizeof(query), "SinglePortForward_%d", idx);
@@ -3707,6 +3711,9 @@ static int do_single_port_forwarding(FILE *nat_fp, FILE *filter_fp, int iptype, 
             }
 #endif
    }
+
+   } // END of if (CosaNAT::port_forward_enabled)
+
 SinglePortForwardNext:
 #if defined (FEATURE_MAPT) || defined (FEATURE_SUPPORT_MAPT_NAT46)
      if(isFeatureDisabled == TRUE)
@@ -3762,6 +3769,10 @@ static int do_port_range_forwarding(FILE *nat_fp, FILE *filter_fp, int iptype, F
    FIREWALL_DEBUG("PortMapping:Feature Enable %d\n" COMMA TRUE);
    isFeatureDisabled = FALSE;
 #endif
+
+   syscfg_get("CosaNAT", "port_forward_enabled", query, sizeof(query));
+   if ((query[0] != '\0') && (atoi(query) != 0))
+   {
 
    for (idx=1 ; idx<=count ; idx++) {
       namespace[0] = '\0';
@@ -4157,6 +4168,9 @@ static int do_port_range_forwarding(FILE *nat_fp, FILE *filter_fp, int iptype, F
 #endif
 
    }
+
+   } // END of if (CosaNAT::port_forward_enabled)
+
 PortRangeForwardNext:
 #if defined (FEATURE_MAPT) || defined (FEATURE_SUPPORT_MAPT_NAT46)
       if (isFeatureDisabled == TRUE)
