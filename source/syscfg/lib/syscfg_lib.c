8a06bc285322190d7e456dc0e86fccc31c69be41
Andre McCurdy <armccurdy@gmail.com>
2020-11-30 18:11:34 -0800

    syscfg: improved error checking + add debug to syscfg_get()
    Generate a clear warning if the output buffer is too small (ie the
    output will be truncated) in syscfg_get(). Plus misc code cleanup.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/syscfg/lib/syscfg_lib.c
+++ source/syscfg/lib/syscfg_lib.c
@@ -33,6 +33,10 @@
    limitations under the License.
 **********************************************************************/
 
+/* _GNU_SOURCE is needed for strchrnul() and program_invocation_short_name */
+
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -50,11 +54,15 @@
 #include <string.h>
 #include <semaphore.h>
 #include <pthread.h>
+#include <assert.h>
+
 #include <ulog/ulog.h>
 #include "syscfg_mtd.h"   // MTD IO friend sub-class
 #include "syscfg_lib.h"   // internal interface
 #include "syscfg.h"       // external interface used by users
 #include "safec_lib_common.h"
+
+#define VERBOSE_DEBUG
 
 /*
  * Global data structures
@@ -67,6 +75,42 @@ static char name_p[MAX_NAME_LEN+1];                      // internal temp name b
 int load_from_file (const char *fname);
 int commit_to_file (const char *fname);
 int backup_file (const char *bkupFile, const char *localFile);
+
+#if defined (VERBOSE_DEBUG)
+
+static void dump_maps (void)
+{
+    char buf[8192];
+    pid_t pid;
+    int fd;
+
+    pid = getpid();
+
+    fd = open ("/proc/self/maps", O_RDONLY);
+
+    if (fd == -1) {
+        fprintf (stderr, "Open /proc/self/maps failed (PID %d)\n", pid);
+    }
+    else {
+        int nread;
+        fprintf (stderr, "/proc/self/maps (PID %d):\n\n", pid);
+        while (1)
+        {
+            nread = read (fd, buf, sizeof(buf) - 1);
+            if (nread == 0)
+                break;
+            buf[nread] = 0;
+            if (buf[0] != 0)
+                fprintf (stderr, "%s", buf);
+        }
+        close (fd);
+        fprintf (stderr, "\n\n");
+    }
+
+    fflush(NULL);
+}
+
+#endif
 
 /******************************************************************************
  *                External syscfg library access apis
@@ -85,17 +129,40 @@ int backup_file (const char *bkupFile, const char *localFile);
  */
 int syscfg_get (const char *ns, const char *name, char *out_val, int outbufsz)
 {
+    char *val;
+    size_t len;
+
+    assert(outbufsz > 1);
+
     if (0 == syscfg_initialized || NULL == name || NULL == out_val) {
+        if (out_val != NULL) {
+            out_val[0] = 0;
+        }
         return -1;
     }
 
-    char *val = _syscfg_get(ns, name);
-    if (val) {
-        strncpy(out_val, val, outbufsz-1);
-        out_val[outbufsz-1] = '\0';
-        return 0;
+    val = _syscfg_get(ns, name);
+
+    if (val == NULL) {
+        out_val[0] = 0;
+        return -1;
     }
-    return -1;
+
+    len = strlen(val);
+
+    if (len >= outbufsz) {
+        memcpy(out_val, val, outbufsz - 1);
+        out_val[outbufsz - 1] = 0;
+#if defined (VERBOSE_DEBUG)
+        fprintf(stderr, "syscfg_get: %s outbufsz too small (%d < %d) (%s: lr %p)\n", name, outbufsz, (int) len + 1, program_invocation_short_name, __builtin_extract_return_addr (__builtin_return_address (0)));
+        dump_maps();
+#endif
+    }
+    else {
+        memcpy(out_val, val, len + 1);
+    }
+
+    return 0;
 }
 
 /*
