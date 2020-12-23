97a6c18270e5057ab1d5903455f17db3eebce191
Andre McCurdy <armccurdy@gmail.com>
2020-12-23 02:47:57 -0800

    utopia_init.sh: drop support for removing lxy L2 dir
    Comcast specific.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/scripts/init/system/utopia_init.sh
+++ source/scripts/init/system/utopia_init.sh
@@ -327,15 +327,6 @@ fi
       rm -f /nvram/pcs.bin
       rm -f /nvram/pcs.bin.md5
    fi
-   # Remove lxy L2 dir
-   LOG_FILE=/rdklogs/logs/lxy.log
-   echo_t "[FR] Removing lxy L2 Dir" >> $LOG_FILE
-   if [ -f /etc/lxy.conf ];then
-       L2="$(grep '^L2=' /etc/lxy.conf | sed -e 's/L2=//')"
-   fi
-   if [ -d "$L2" ]; then
-       rm -rf $L2
-   fi
 
      touch /nvram/.apply_partner_defaults   
    #>>zqiu
