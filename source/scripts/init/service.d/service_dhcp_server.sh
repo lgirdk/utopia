48050a61e04a738c6b751cb732757d83d642d308
qlei <rlei@libertyglobal.com>
2020-12-23 12:47:41 -0800

    call c code via sysevent instead of script to avoid dnsmasq is configured by c code and script at the same time

--- source/scripts/init/service.d/service_dhcp_server.sh
+++ source/scripts/init/service.d/service_dhcp_server.sh
@@ -858,7 +858,9 @@ case "$1" in
             if [ "$BOX_TYPE" = "XB3" ]; then
                 service_dhcp lan-status started lan_not_restart
             else
-                lan_status_change started lan_not_restart
+                #lan_status_change started lan_not_restart
+                #20201223 change from script to c code to avoid dnsmasq is configured by scrip and c code at the same time. but sysevent can't pass two paras
+                sysevent set lan-status started
             fi
         fi
       ;;
