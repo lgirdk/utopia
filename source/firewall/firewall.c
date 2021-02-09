cbfe58abbdd4353d4a24e60de77993881a5f7d9a
Andre McCurdy <armccurdy@gmail.com>
2021-02-08 21:01:42 -0800

    ensure that all XDNS related code is conditional on XDNS_ENABLE
    XDNS (ie routing of DNS requests from the LAN through dnsmasq running
    on the router) needs to be both enabled at build time (via the
    XDNS_ENABLE macro) and run time (via the X_RDKCENTRAL-COM_XDNS syscfg
    option and by setting XDNS_ENABLE="true" in /etc/device.properties).
    
    By default, the build time option is enabled but the run-time options
    are not, so XDNS support is disabled.
    
    Since XDNS isn't currently used, update the code to fully disable
    XDNS support at build time if XDNS_ENABLE is not defined. A separate
    change to remove XDNS_ENABLE from the global CFLAGS will then fully
    disable the feature.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/firewall/firewall.c
+++ source/firewall/firewall.c
@@ -7451,6 +7451,7 @@ static int do_device_based_parcon(FILE *natFp, FILE* filterFp)
 /*
 ** XDNS - Route DNS requests from LAN through dnsmasq.
 **/
+#ifdef XDNS_ENABLE
 static int do_dns_route(FILE *nat_fp, int iptype) {
 
 	char xdnsflag[20] = {0};
@@ -7535,6 +7536,7 @@ static int do_dns_route(FILE *nat_fp, int iptype) {
 
 	return 0;
 }
+#endif
 
 
 /*
