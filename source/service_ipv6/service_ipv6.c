c540e528539ff6ea14c516a4472fdff5f7a5e515
Andre McCurdy <armccurdy@gmail.com>
2021-02-10 21:54:48 -0800

    service_ipv6: control dibbler log level based on a syscfg variable ( OFW-2787 F3896SI-1072 )
    Size of dibbler-client.log increases with every wan-restart
    
    BUG DESCRIPTION:
    size of dibbler log file increases for wan-restart since there is no scenario for handling the log rotation.
    
    FIX DESCRIPTION:
    Introduce dibbler_log_level syscfg variable to set dibbler log-level
    in configuration file and set default log level as 4 (Error) in
    syscfg db. This syscfg variable can be changed to other log-level for
    debugging purpose.
    
    From the dibbler documentation ( https://klub.com.pl/dhcpv6/doc/dibbler-user.pdf ):
    
      log-level:
    
      Takes one integer parameter. Defines verbose level of the log
      messages. The valid range is from 1 (very quiet) to 8 (very
      verbose). Those values are modelled after levels used in syslog.
      These are:
    
        1 : Emergency
        2 : Alert
        3 : Critical
        4 : Error
        5 : Warning
        6 : Notice
        7 : Info
        8 : Debug
    
      Currently Dibbler is using levels 3 to 8, as 1 and 2 are reserved
      for system wide emergency event
    
    TESTING PERFORMED:
    changed dibbler_log_level syscfg varibale and tested it for different log-level.
    
    Signed-off-by: Saravanakumar Karthikeyan <skarthikeyan.contractor@libertyglobal.com>

--- source/service_ipv6/service_ipv6.c
+++ source/service_ipv6/service_ipv6.c
@@ -1509,7 +1509,36 @@ static int gen_dibbler_conf(struct serv_ipv6 *si6)
         return -1;
 
     /*Begin write dibbler configurations*/
-    fprintf(fp, "log-level 4\n");
+
+    {
+        char buf[12];
+        int log_level = 0;
+
+        if (syscfg_get( NULL, "dibbler_log_level", buf, sizeof(buf)) == 0)
+        {
+            log_level = atoi(buf);
+        }
+        if (log_level < 1)
+        {
+            log_level = 1;
+        }
+        else if (log_level > 8)
+        {
+            log_level = 4;
+        }
+
+        /*
+            1 : Emergency (not used - logging will be disabled)
+            2 : Alert (not used - logging will be disabled)
+            3 : Critical
+            4 : Error
+            5 : Warning
+            6 : Notice
+            7 : Info
+            8 : Debug
+        */
+        fprintf(fp, "log-level %d\n", log_level);
+    }
 
     /*
        Enable inactive mode: When server begins operation and it detects that
