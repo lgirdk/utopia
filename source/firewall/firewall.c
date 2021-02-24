1b56b35734e5b0d52c50926edb76422081ff6c58
anil shetty <adshetty@libertyglobal.com>
2021-02-23 20:57:37 -0800

    firewall: allow UI to work even if the LAN client is set to block on MAC filter ( OFW-716 )

--- source/firewall/firewall.c
+++ source/firewall/firewall.c
@@ -15423,7 +15423,6 @@ static int do_mac_filter(FILE *fp)
                 rc = syscfg_get(namespace, "mac", MACAddress, sizeof(MACAddress));
                 if ( (rc != 0) || ('\0' != MACAddress[0]) )
                 {
-                    fprintf(fp, "-I INPUT -m mac --mac-source %s -j DROP\n", MACAddress);
                     fprintf(fp, "-I FORWARD -m mac --mac-source %s -j DROP\n", MACAddress);
 
                 }
