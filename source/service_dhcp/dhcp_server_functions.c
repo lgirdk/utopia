8f37ce173335085f8d4b366c375972a7b41d1375
Tarun Saha <tsaha@libertyglobal.com>
2021-03-01 14:29:20 -0800

    service_dhcp: bind to port 53 on LAN interfaces only ( OFW-25 )
    Close port 53 on the wan interface.

--- source/service_dhcp/dhcp_server_functions.c
+++ source/service_dhcp/dhcp_server_functions.c
@@ -1056,6 +1056,7 @@ int prepare_dhcp_conf (char *input)
     // Dont add resolv-file if in norf captive portal mode
     if(FALSE == l_bRfCp)
     {
+        fprintf(l_fLocal_Dhcp_ConfFile, "bind-interfaces\n");
         fprintf(l_fLocal_Dhcp_ConfFile, "domain-needed\n");
         fprintf(l_fLocal_Dhcp_ConfFile, "bogus-priv\n");
 
