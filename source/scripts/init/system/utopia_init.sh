5743264638a46e7fed3cc9a5be70436806450786
Cibil <cpankiras.contractor@libertyglobal.com>
2021-03-12 15:20:51 +0100

    add support for setting Customer index based on platform specific config ( OFW-570 )
    When we upgrade mv1 to mng, we need to convert the existing operator id to customer index. Since many countries are using the same operator id, it is not unique.
    But country id is unique in mv1. So during the first upgrade or a factory reset, read the country id from CbnDocsisDb and write it in syscfg.
    Also made changes in syscfg to handle customer index change via dmcli/boot-cofig-file without factory reset.
    
    The value of customer-index-changed is changed from True to true so the extra check can be removed.
    
    Testing Performed:
    1. Factory reset the DUT
    2. Device.X_LGI-COM_General.CustomerId should contain a value other than zero
    3. Change Device.X_LGI-COM_General.CustomerId via dmcli
    4. syscfg and psm db values should change according to the new customer index
    
    Signed-off-by: Cibil <cpankiras.contractor@libertyglobal.com>
    
    syscfg: only set "customer-index-changed" to true corresponding customerId specific file exists
    
    Prevent changes to /nvram/bbhm_cur_cfg.xml getting lost after a
    reboot when customerId is 0.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    
    Map OperatorId to Customer index ( OFW-570 )
    
    Moved the logic of converting CountryId to Customer Index to platform hal
    
    Signed-off-by: Cibil <cpankiras.contractor@libertyglobal.com>
    
    syscfg: misc code cleanup etc
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/scripts/init/system/utopia_init.sh
+++ source/scripts/init/system/utopia_init.sh
@@ -116,6 +116,7 @@ PSM_BAK_XML_CONFIG_FILE_NAME="$SYSCFG_MOUNT/bbhm_bak_cfg.xml"
 PSM_TMP_XML_CONFIG_FILE_NAME="$SYSCFG_MOUNT/bbhm_tmp_cfg.xml"
 XDNS_DNSMASQ_SERVERS_CONFIG_FILE_NAME="$SYSCFG_MOUNT/dnsmasq_servers.conf"
 FACTORY_RESET_REASON=false
+CUSTOMER_BOOT_CONFIG_FILE="/nvram/bootconfig_custindex"
 
 HOTSPOT_BLOB="/nvram/hotspot_blob"
 if [ -d $SYSCFG_ENCRYPTED_PATH ]; then
@@ -164,6 +165,12 @@ CheckAndReCreateDB()
 
 echo_t "[utopia][init] Starting syscfg using file store ($SYSCFG_BKUP_FILE)"
 if [ -f $SYSCFG_BKUP_FILE ]; then
+
+   if [ -f $CUSTOMER_BOOT_CONFIG_FILE ]; then
+      # If Customer index is set via boot config then remove /nvram/sycfg.db file
+      echo -n > $SYSCFG_BKUP_FILE
+   fi
+
    cp $SYSCFG_BKUP_FILE $SYSCFG_FILE
    syscfg_create -f $SYSCFG_FILE
    if [ $? != 0 ]; then
@@ -316,6 +323,13 @@ elif [ "$FACTORY_RESET_WIFI" = "$SYSCFG_FR_VAL" ]; then
 fi
 #echo_t "[utopia][init] Cleaning up vendor nvram"
 # /etc/utopia/service.d/nvram_cleanup.sh
+
+# In case customer index and factory reset happens at the same time,
+# syscfg_create is called for two times. So remove bootconfig_custindex file after that.
+if [ -f $CUSTOMER_BOOT_CONFIG_FILE ]; then
+    # Remove /nvram/bootconfig_custindex file. Customer specific values are already added in syscfg.
+    rm -f $CUSTOMER_BOOT_CONFIG_FILE
+fi
 
 #CISCOXB3-6085:Removing current configuration from nvram as a part of PSM migration.
 if [ -f /nvram/bbhm_cur_cfg.xml  ]; then
