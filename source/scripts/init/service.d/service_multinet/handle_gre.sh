caad17e4f487b76e2f62b03a504f2a0a1bc73130
Andre McCurdy <armccurdy@gmail.com>
2021-03-24 17:20:28 -0700

    add initial support for MV2PLUS box type
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/scripts/init/service.d/service_multinet/handle_gre.sh
+++ source/scripts/init/service.d/service_multinet/handle_gre.sh
@@ -534,7 +534,7 @@ update_bridge_frag_config () {
         if [ "$AB_SSID_DELIM" = $br ]; then
             continue
         fi
-   if [ "$BOX_TYPE" = "XB6" ] ; then
+   if [ "$BOX_TYPE" = "XB6" ] || [ "$BOX_TYPE" = "MV2PLUS" ]; then
         echo add br=$br mtu=$MTU_VAL icmp=y segment=y gw=$2 > /proc/net/mtu_mod
    fi
     done
@@ -967,7 +967,7 @@ case "$1" in
                        ifconfig gretap0 up
                fi
          fi
-    if [ "$BOX_TYPE" = "XB6" ] ; then
+    if [ "$BOX_TYPE" = "XB6" ] || [ "$BOX_TYPE" = "MV2PLUS" ] ; then
         read_greInst 
     fi
     if [ $recover != "true" ] ; then                            
@@ -998,7 +998,7 @@ case "$1" in
             ifconfig ${GRE_IFNAME} down
             sysevent set if_${GRE_IFNAME}-status $IF_DOWN
         fi
-        if [ "$BOX_TYPE" != "XB6" -o "$MODEL_NUM" = "TG4482A" ] && [ "$BOX_TYPE" != "TCCBR" ]; then
+        if [ "$BOX_TYPE" != "XB6" -o "$MODEL_NUM" = "TG4482A" ] && [ "$BOX_TYPE" != "TCCBR" ] && [ "$BOX_TYPE" != "MV2PLUS" ]; then
            check_ssids
         fi
       fi
@@ -1093,7 +1093,7 @@ case "$1" in
             if [ xstarted = x$hotspot_started ]; then
                 if [ x != x$curr_tunnel ]; then
                     create_tunnel $curr_tunnel $name
-                    if [ "$BOX_TYPE" = "XB6" ] || [ "$BOX_TYPE" = "TCCBR" ] ; then                                    
+                    if [ "$BOX_TYPE" = "XB6" ] || [ "$BOX_TYPE" = "TCCBR" ] || [ "$BOX_TYPE" = "MV2PLUS" ] ; then
                         read_greInst                                                                    
                         check_ssids 1                                                     
                     fi   
