b0ee9e44f33288f91c5e0e25bf6d3f391388818c
Andre McCurdy <armccurdy@gmail.com>
2021-03-24 17:20:28 -0700

    add initial support for MV2PLUS box type
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/scripts/init/service.d/service_dhcp_server.sh
+++ source/scripts/init/service.d/service_dhcp_server.sh
@@ -537,7 +537,7 @@ dhcp_server_start ()
    if [ $? -eq 0 ]; then
    	echo_t "$SERVER process started successfully"
    else
-   	if [ "$BOX_TYPE" = "XB6" ] || [ "$BOX_TYPE" = "PUMA7_CGP" ] || [ "$BOX_TYPE" = "rpi" ] ; then
+   	if [ "$BOX_TYPE" = "XB6" ] || [ "$BOX_TYPE" = "PUMA7_CGP" ] || [ "$BOX_TYPE" = "rpi" ] || [ "$BOX_TYPE" = "MV2PLUS" ]; then
    
         	COUNTER=0
         	while [ $COUNTER -lt 5 ]; do
@@ -748,7 +748,7 @@ dns_start ()
 	 if [ $? -eq 0 ]; then
    		echo_t "$SERVER process started successfully"
    	 else
-   		if [ "$BOX_TYPE" = "XB6" ] || [ "$BOX_TYPE" = "PUMA7_CGP" ]; then
+   		if [ "$BOX_TYPE" = "XB6" ] || [ "$BOX_TYPE" = "PUMA7_CGP" ] || [ "$BOX_TYPE" = "MV2PLUS" ]; then
    
         		COUNTER=0
         		while [ $COUNTER -lt 5 ]; do
@@ -840,7 +840,7 @@ case "$1" in
     ipv4_*-status)
         if [ x"up" = x$2 ]; then
 	        echo_t "SERVICE DHCP : Got ipv4 status"
-            if [ "$BOX_TYPE" = "XB3" ] || [ "$BOX_TYPE" = "MV1" ]; then
+            if [ "$BOX_TYPE" = "XB3" ] || [ "$BOX_TYPE" = "MV1" ] || [ "$BOX_TYPE" = "MV2PLUS" ]; then
 		#setting lan_status-dhcp to started for handling dhcp_server-restart in brige mode
 		sysevent set lan_status-dhcp started
 		echo_t "SERVICE DHCP : $1, calling dhcp_server-restart lan_not_restart event"
