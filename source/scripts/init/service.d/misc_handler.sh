f36c6d8c64fa8443c157e2ddc97117e2ba5cf990
Tarun Saha <tsaha@libertyglobal.com>
2021-04-12 13:04:23 -0700

    replace igmpproxy and mldproxy with mcproxy
    igmpproxy is replaced with mcproxy_v4 (alias for mcproxy)
    mldproxy is replaced with mcproxy_v6 (alias for mcproxy)

--- source/scripts/init/service.d/misc_handler.sh
+++ source/scripts/init/service.d/misc_handler.sh
@@ -97,8 +97,8 @@ then
     rm /etc/cron/cron.everyminute/misc_handler.sh
 else
     RG_MD=`syscfg get last_erouter_mode`
-    MLD_PID=`pidof mldproxy`
-    IGMP_PID=`pidof igmpproxy`
+    MLD_PID=`pidof mcproxy_v6`
+    IGMP_PID=`pidof mcproxy_v4`
 
     if [ "$IGMP_PID" = "" ]; then
       if [ "$RG_MD" = "1" -o "$RG_MD" = "3" ]; then
