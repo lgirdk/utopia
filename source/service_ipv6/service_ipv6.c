1dfd4201ba4ee4703737ba9d7029f7d5551c541b
BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
2021-05-03 18:59:07 +0200

    dibbler server conf is not getting updated with public DNS updates ( Part of OFW-1127 )
    DESCRIPTION:-
    
    If syscfg "dns_override" is true (ie certain Customer IDs only) when
    public DNS configurations are changed from UI or via dmcli
    /etc/dibbler/server.conf is not getting updated and a newly connected
    client will still receive older dns server IP.
    
    TESTING PERFORMED:-
    
    1. Loaded the modem with a customer index which allows dns_override to be enabled.
    2. Enabled Public DNS from UI after updating the dns server Ip's.
    3. Connected a lan client and verified that /etc/resolv.conf is getting updated values.
    4. Changed the IP configuraions from UI/dmcli and verified that /var/zebra.conf, /etc/dibbler/server.conf and connected clients are getting updated with new values.
    
    Signed-off-by: Bijeesha <bvattakaithayilsudha.contractor@libertyglobal.com>

--- source/service_ipv6/service_ipv6.c
+++ source/service_ipv6/service_ipv6.c
@@ -1782,6 +1782,9 @@ OPTIONS:
     else {
 	sysevent_set(si6->sefd, si6->setok, "dibbler_server_conf-status", "ready", 0);
     }     
+
+    system("/etc/utopia/service.d/set_ipv6_dns.sh dibbler");
+
     return 0;
 }
 
