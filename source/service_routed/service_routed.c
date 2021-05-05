b0adb5e05672023d46c3cc133cbe13a0b89d2540
Sarveswaran Nagarajan <sanagarajan.contractor@libertyglobal.com>
2021-05-05 06:08:50 +0200

    fix Router Advertisements do not contain DNSSL option ( OFW-281 )
    Bug Description: Router Advertisements do not contain DNSSL option
    
    Fix Description: Moved the code block out of captive portal check
    
    Testing Performed : Executed the  test ipv6_ndp_33 CDRouter test
    
    Signed-off-by: Sarveswaran Nagarajan <sanagarajan.contractor@libertyglobal.com>

--- source/service_routed/service_routed.c
+++ source/service_routed/service_routed.c
@@ -1177,6 +1177,7 @@ static int gen_zebra_conf(int sefd, token_t setok)
                     #endif
 				}
 			}
+		}
 
                 if (atoi(valid_lft) <= (3 * ra_interval))
                 {
@@ -1194,8 +1195,6 @@ static int gen_zebra_conf(int sefd, token_t setok)
                     fprintf(fp, "   ipv6 nd dnssl %s %s\n", tok, dnssl_lft);
                 }
 
-
-		}
 	}
     
 
