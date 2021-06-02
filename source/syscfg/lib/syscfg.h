7412790eda914085493fe7d471b06758906460e6
Andre McCurdy <armccurdy@gmail.com>
2021-06-01 23:02:31 -0700

    syscfg: set size limit for persistent backup file to 60k for all targets
    With only default values, the syscfg persistent backup file is approx
    18k. It's not clear how many more values get added dynamically at run
    time but 60k gives quite some space. Note that the upstream default
    was 50k for a long time. It was changed to 100k fairly recently, with
    no clear explanation.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/syscfg/lib/syscfg.h
+++ source/syscfg/lib/syscfg.h
@@ -36,13 +36,7 @@
 #ifndef _SYSCFG_H_
 #define _SYSCFG_H_
 
-// Changing sysconfig size to 100kb for non XB3 devices
-
-#ifdef _COSA_INTEL_XB3_ARM_
-	#define SYSCFG_SZ (50 * (1024))       /* max file size - 50kb */
-#else
-	#define SYSCFG_SZ (100 * (1024))       /* max file size - 100kb */
-#endif
+#define SYSCFG_SZ (60 * 1024)
 
 #define ERR_INVALID_PARAM     -1
 #define ERR_NOT_INITIALIZED   -2
