393e84b6ba9a1440027d08abfe6669db4fc1f07f
Tarun Saha <tsaha@libertyglobal.com>
2021-06-07 12:18:26 -0700

    service_dhcp: use message_bus apis to replace dmcli system calls ( OFW-1153 )
    Reduce CPU load during bootup.

--- source/service_dhcp/include/dhcp_server_functions.h
+++ source/service_dhcp/include/dhcp_server_functions.h
@@ -25,7 +25,7 @@ void prepare_dhcp_conf_static_hosts();
 void prepare_dhcp_options_wan_dns();
 void prepare_whitelist_urls(FILE *);
 void do_extra_pools (FILE *local_dhcpconf_file, char *prefix, unsigned char bDhcpNs_Enabled, char *pWan_Dhcp_Dns);
-int prepare_dhcp_conf();
+int prepare_dhcp_conf(char *input, void *bus_handle);
 void check_and_get_wan_dhcp_dns( char *pl_cWan_Dhcp_Dns );
 void get_dhcp_option_for_brlan0( char *pDhcpNs_OptionString );
 void prepare_static_dns_urls(FILE *fp_local_dhcp_conf);
