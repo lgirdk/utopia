d983a5c1c316ec685ccbab71d79236c1847306ac
Andre McCurdy <armccurdy@gmail.com>
2021-11-16 17:12:48 -0800

    drop utctx_cmd SYSCFG_FAILED error handling
    If utctx_cmd sets SYSCFG_FAILED the it either means utctx_cmd was
    called with invalid command line options or syscfg failed (ie it's
    not a routine error case such as syscfg value not found etc).
    
    Trying to handle SYSCFG_FAILED complicates future updates to remove
    the need for "eval" with utctx_cmd (and there's not much a script
    calling utctx_cmd can do in response anyway) so simplify the code and
    drop attempts to handle it.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/scripts/init/service.d/service_system.sh
+++ source/scripts/init/service.d/service_system.sh
@@ -52,18 +52,8 @@ service_init ()
 {
    # Get all provisioning data
 
-   SYSCFG_FAILED='false'
    FOO=`utctx_cmd get wan_physical_ifname wan_virtual_ifnum lan_ifname`
    eval "$FOO"
-   if [ $SYSCFG_FAILED = 'true' ] ; then
-      ulog system status "$PID utctx failed to get some configuration data required by service-system"
-      ulog system status "$PID THE SYSTEM IS NOT SANE"
-      echo "[utopia] utctx failed to get some configuration data required by service-system" > /dev/console
-      echo "[utopia] THE SYSTEM IS NOT SANE" > /dev/console
-      sysevent set ${SERVICE_NAME}-status error
-      sysevent set ${SERVICE_NAME}-errinfo "Unable to get crucial information from syscfg"
-      exit
-   fi
 }
 
 #--------------------------------------------------------------
