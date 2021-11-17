e7c0182dcf0ecc015cc02465fee6706234043339
Andre McCurdy <armccurdy@gmail.com>
2021-11-16 17:12:48 -0800

    drop utctx_cmd SYSCFG_FAILED error handling
    If utctx_cmd sets SYSCFG_FAILED the it either means utctx_cmd was
    called with invalid command line options or syscfg failed (ie it's
    not a routine error case such as syscfg value not found etc).
    
    Trying to handle SYSCFG_FAILED complicates future updates to remove
    the need for "eval" with utctx_cmd (and there's not much a script
    calling utctx_cmd can do in response anyway) so simplify the code and
    drop attempts to handle it.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/scripts/init/service.d/service_forwarding.sh
+++ source/scripts/init/service.d/service_forwarding.sh
@@ -72,18 +72,8 @@ router_mode ()
 #--------------------------------------------------------------
 service_init ()
 {
-   SYSCFG_FAILED='false'
    FOO=`utctx_cmd get bridge_mode`
    eval $FOO
-   if [ "$SYSCFG_FAILED" = 'true' ] ; then
-      ulog forwarding status "$PID utctx failed to get some configuration data required by service-forwarding"
-      ulog forwarding status "$PID THE SYSTEM IS NOT SANE"
-      echo "[utopia] utctx failed to get some configuration data required by service-system" > /dev/console
-      echo "[utopia] THE SYSTEM IS NOT SANE" > /dev/console
-      sysevent set ${SERVICE_NAME}-status error
-      sysevent set ${SERVICE_NAME}-errinfo "Unable to get crucial information from syscfg"
-      exit
-   fi
 }
 
 #--------------------------------------------------------------
