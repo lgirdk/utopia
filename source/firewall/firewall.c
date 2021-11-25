d443352afea67f7af40404d0a94b6c95c2145d55
Cibil <cpankiras.contractor@libertyglobal.com>
2021-11-25 12:47:02 +0100

    RIPv2: Impact on filtering rules when single static IP address is enabled ( MVXREQ-1799 )
    [Description]
    
    When single static IP address is enabled, any existing filtering rules
    (port forwarding, DMZ, port triggering,IP and Port Filtering), then these rules must persist,
    as long as they are valid (i.e., the source and destinations as per the filtering rules
    requirements are bound to the /32 static IP address)
    
    [Testing Performed]
    
    1. Create some port forwarding and dmz rules from UI
    
    2. Enable RIP feautre
    dmcli eRT setv Device.Routing.RIP.Enable bool true
    
    3. Set Static IP to erouter0
    dmcli eRT addtable Device.IP.Interface.1.IPv4Address.
    dmcli eRT setv Device.IP.Interface.1.IPv4Address.2.Alias string rip_alias
    dmcli eRT setv Device.IP.Interface.1.IPv4Address.2.IPAddress string 84.116.12.48
    dmcli eRT setv Device.IP.Interface.1.IPv4Address.2.SubnetMask string 255.255.255.255
    
    4. Port forwarding and dmz rules must be mapped with static IP
    
    Signed-off-by: Cibil <cpankiras.contractor@libertyglobal.com>

--- source/firewall/firewall.c
+++ source/firewall/firewall.c
@@ -597,6 +597,8 @@ static int do_wpad_isatap_blockv4(FILE *fp);
 static int do_wpad_isatap_blockv6(FILE* fp);
 //LGI Add End
 
+static int get_erouter_secondary_ip(char *ip, int size);
+
 FILE *firewallfp = NULL;
 
 #define CONFIG_BUILD_TRIGGER 1
@@ -3302,6 +3304,7 @@ static int do_single_port_forwarding(FILE *nat_fp, FILE *filter_fp, int iptype, 
    char query[MAX_QUERY];
    int  rc;
    int  count;
+   int nat_swapped = 0;
 #ifndef INTEL_PUMA7
    char *tmp = NULL;
 #endif
@@ -3329,6 +3332,11 @@ static int do_single_port_forwarding(FILE *nat_fp, FILE *filter_fp, int iptype, 
        isFeatureDisabled = FALSE;
    }
 #endif
+
+   /* If rip is enabled and erouter has static IP then swap the NAT IP. */
+   char temp_natip4[20];
+   memcpy(temp_natip4, natip4, sizeof(temp_natip4));
+   nat_swapped = get_erouter_secondary_ip(natip4, sizeof(natip4));
 
    //LGI ADD START
    rc = syscfg_get(NULL, "CosaNAT::port_forward_enabled", query, sizeof(query));
@@ -3685,7 +3693,13 @@ SinglePortForwardNext:
          FIREWALL_DEBUG("PortMapping:Feature Enable %d\n" COMMA FALSE);
      }
 #endif
-           FIREWALL_DEBUG("Exiting do_single_port_forwarding\n");       
+   if(nat_swapped)
+   {
+      /* swap the NAT address back to DHCP IP */
+      memcpy(natip4, temp_natip4, sizeof(natip4));
+   }
+   FIREWALL_DEBUG("Exiting do_single_port_forwarding\n");
+
    return(0);
 }
 
@@ -3711,6 +3725,7 @@ static int do_port_range_forwarding(FILE *nat_fp, FILE *filter_fp, int iptype, F
    BOOL isFeatureDisabled = TRUE;
 #endif
    BOOL varyingPortRange = TRUE; // LGI ADD
+   int nat_swapped = 0;
 
 #ifdef CISCO_CONFIG_TRUE_STATIC_IP 
 
@@ -3734,6 +3749,11 @@ static int do_port_range_forwarding(FILE *nat_fp, FILE *filter_fp, int iptype, F
    FIREWALL_DEBUG("PortMapping:Feature Enable %d\n" COMMA TRUE);
    isFeatureDisabled = FALSE;
 #endif
+
+   /* If rip is enabled and erouter has static IP then swap the NAT IP. */
+   char temp_natip4[20];
+   memcpy(temp_natip4, natip4, sizeof(temp_natip4));
+   nat_swapped = get_erouter_secondary_ip(natip4, sizeof(natip4));
 
    //LGI ADD START
    rc = syscfg_get(NULL, "CosaNAT::port_forward_enabled", query, sizeof(query));
@@ -4259,6 +4279,12 @@ PortRangeForwardNext:
       }
 #endif
 
+   if (nat_swapped)
+   {
+      /* swap the NAT address back to DHCP IP */
+      memcpy(natip4, temp_natip4, sizeof(natip4));
+   }
+
          FIREWALL_DEBUG("Exiting do_port_range_forwarding\n");
 
    return(0);
@@ -4780,6 +4806,7 @@ static int do_dmz(FILE *nat_fp, FILE *filter_fp)
 
    int rc;
    int  src_type = 0; // 0 is all networks, 1 is an ip[/netmask], 2 is ip range
+   int nat_swapped = 0;
            FIREWALL_DEBUG("Entering do_dmz\n");       
    if (!isDmzEnabled) {
       return(0);
@@ -4904,6 +4931,11 @@ if(status_http_ert == 0){
    /* tohost is now a full ip address */
    snprintf(dst_str, sizeof(dst_str), "--to-destination %s ", tohost);
 
+   /* If rip is enabled and erouter has static IP then swap the NAT IP. */
+   char temp_natip4[20];
+   memcpy(temp_natip4, natip4, sizeof(temp_natip4));
+   nat_swapped = get_erouter_secondary_ip(natip4, sizeof(natip4));
+
    switch (src_type) {
       case(0):
          if (isNatReady &&
@@ -4998,7 +5030,13 @@ if(status_http_ert == 0){
       default:
          break;
    }
-           FIREWALL_DEBUG("Exiting do_dmz\n");       
+   if(nat_swapped)
+   {
+      /* swap the NAT address back to DHCP IP */
+      memcpy(natip4, temp_natip4, sizeof(natip4));
+   }
+   FIREWALL_DEBUG("Exiting do_dmz\n");
+
    return(0);
 }
 
@@ -18238,3 +18276,25 @@ static int do_wpad_isatap_blockv6 (FILE* filter_fp)
 }
 //LGI END
 
+/*
+Api to retrieve erouter0's static ip
+reutrn : 1 if erouter0 has static ip else 0
+*/
+static int get_erouter_secondary_ip(char *ip, int size)
+{
+   char erouter_static_enable[8];
+   char erouter_static_ip[20];
+   if(isRipEnabled)
+   {
+      syscfg_get(NULL, "erouter_static_ip_enable", erouter_static_enable, sizeof(erouter_static_enable));
+      if(strcmp(erouter_static_enable, "true") == 0)
+      {
+         if(syscfg_get(NULL, "erouter_static_ip_address", erouter_static_ip, sizeof(erouter_static_ip)) == 0)
+         {
+            memcpy(ip,erouter_static_ip,size);
+            return 1;
+         }
+      }
+   }
+   return 0;
+}
