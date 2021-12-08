1eccc3f761923745a3640c1227c283d3052f7102
Andre McCurdy <armccurdy@gmail.com>
2021-12-07 18:53:26 -0800

    Revert "RDKB-38376, RDKB-35029 : Unknown event: meshethbhaul-bridge-setup" ( ** TMP ** )
    This reverts commit b1dc876608fc5131dcb26fb54e88bda0f9b0f788.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/scripts/init/c_registration/02_multinet.c
+++ source/scripts/init/c_registration/02_multinet.c
@@ -115,6 +115,7 @@ const char* SERVICE_CUSTOM_EVENTS[] = {
     "multinet-up|/etc/utopia/service.d/vlan_util_tchxb6.sh|NULL|"TUPLE_FLAG_EVENT,
     "lnf-setup|/etc/utopia/service.d/vlan_util_tchxb6.sh|NULL|"TUPLE_FLAG_EVENT,
     "meshbhaul-setup|/etc/utopia/service.d/vlan_util_tchxb6.sh|NULL|"TUPLE_FLAG_EVENT,
+    "meshethbhaul-bridge-setup|/etc/utopia/service.d/vlan_util_tchxb6.sh|NULL|"TUPLE_FLAG_EVENT,
     "meshethbhaul-up|/etc/utopia/service.d/vlan_util_tchxb6.sh|NULL|"TUPLE_FLAG_EVENT,
     "meshethbhaul-down|/etc/utopia/service.d/vlan_util_tchxb6.sh|NULL|"TUPLE_FLAG_EVENT,
     NULL };
@@ -128,6 +129,7 @@ const char* SERVICE_CUSTOM_EVENTS[] = {
     "multinet-up|/etc/utopia/service.d/vlan_util_xb7.sh|"ACTION_FLAG_NOT_THREADSAFE"|"TUPLE_FLAG_EVENT,
     "lnf-setup|/etc/utopia/service.d/vlan_util_xb7.sh|"ACTION_FLAG_NOT_THREADSAFE"|"TUPLE_FLAG_EVENT,
     "meshbhaul-setup|/etc/utopia/service.d/vlan_util_xb7.sh|"ACTION_FLAG_NOT_THREADSAFE"|"TUPLE_FLAG_EVENT,
+    "meshethbhaul-bridge-setup|/etc/utopia/service.d/vlan_util_xb7.sh|"ACTION_FLAG_NOT_THREADSAFE"|"TUPLE_FLAG_EVENT,
     "meshethbhaul-up|/etc/utopia/service.d/vlan_util_xb7.sh|"ACTION_FLAG_NOT_THREADSAFE"|"TUPLE_FLAG_EVENT,
     "meshethbhaul-down|/etc/utopia/service.d/vlan_util_xb7.sh|"ACTION_FLAG_NOT_THREADSAFE"|"TUPLE_FLAG_EVENT,
 #else
