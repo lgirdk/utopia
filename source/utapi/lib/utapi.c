9e0d5b45f74b463b50886de68c55badccc4b98d9
shashwat <shsrivastava.contractor@libertyglobal.com>
2021-12-09 10:53:19 +0100

    allow PPTPPassthrough to be enabled/disabled via syscfg ( OFW-2152 )
    BUG DESCRIPTION:
    Value of Device.X_CISCO_COM_Security.Firewall.PPTPPassthrough should be false for customer IDs 50 and 51 instead of true.
    
    FIX DESCRIPTION:
    Added changes to assign the value of PPTPPassthrough from syscfg db to PPTPPassthrough parameter after reboot
    
    TESTING PERFORMED:
    
    1.Factory reset the Device under test
    2.Change customer index to 50
    3.GPV on Device.X_CISCO_COM_Security.Firewall.PPTPPassthrough returns false.
    4.Change customer index to 51
    5.GPV on Device.X_CISCO_COM_Security.Firewall.PPTPPassthrough returns false.
    
    Signed-off-by: Shashwat Srivastava <shsrivastava.contractor@libertyglobal.com>

--- source/utapi/lib/utapi.c
+++ source/utapi/lib/utapi.c
@@ -2059,6 +2059,7 @@ int Utopia_SetFirewallSettings (UtopiaContext *ctx, firewall_t fw)
 int Utopia_GetFirewallSettings (UtopiaContext *ctx, firewall_t *fw)
 {
     int rule_count;
+    int i;
     char buf[8];
     
     bzero(fw, sizeof(firewall_t));
@@ -2102,7 +2103,10 @@ int Utopia_GetFirewallSettings (UtopiaContext *ctx, firewall_t *fw)
     }
 
     fw->allow_ipsec_passthru = TRUE;
-    fw->allow_pptp_passthru = TRUE;
+
+    syscfg_get(NULL, "PPTPPassthrough", buf, sizeof(buf));
+    fw->allow_pptp_passthru = (strcmp(buf, "0") == 0) ? FALSE : TRUE;
+
     fw->allow_l2tp_passthru = TRUE;
     fw->allow_ssl_passthru = TRUE;
 
