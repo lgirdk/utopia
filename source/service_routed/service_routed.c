b1488181cf91173928b8f84ccf2a48074f90e9bb
SreejaSKumar <srekumar.contractor@libertyglobal.com>
2021-12-17 14:51:38 +0530

    service_routed: update default values for ra_interval and ra_lifetime ( OFW-2216 F3896LG-646 )
    Fix default Values of Router Advertisement lifetime and Router Advertisement Interval are incorrect in UI.
    
    Default values of DHCPv6 Parameters(Router Advertisement Lifetime & Router Advertisement Interval) are incorrect in UI.
    
    Steps to Reproduce:
    1. Provision the modem in DSLite mode.
    2. Login to WebUI.
    3. Navigate to advanced --> DHCP
    4. Check the default values of Router Advertisement Lifetime & Router Advertisement Interval.
    
    Actual Result: Default values of router advertisement lifetime and router advertisement interval are 180 & 6 respectively.
    Expected Result: Default values of router advertisement lifetime and router advertisement interval should be 1800 & 180 respectively.
    
    Fix Description:
    Modified the  Router Advertisement Lifetime & Router Advertisement Interval in system defaults.
    
    Testing Perfromed:
    Checked the value using dmcli commands an confirmed the value in UI.
    
    Signed-off-by: Sreeja S Kumar (srekumar.contractor@libertyglobal.com)
    
    fix default value of Router Advertisement Interval should be 180 ( OFW-2248 )
    
    Using default value of 180 for advertisement interval.

--- source/service_routed/service_routed.c
+++ source/service_routed/service_routed.c
@@ -603,7 +603,7 @@ static int gen_zebra_conf(int sefd, token_t setok)
     }
     FILE *fp = NULL;
     char rtmod[16], ra_en[16], dh6s_en[16];
-    int ra_interval;
+    int ra_interval, ra_lifetime;
     char name_servs[1024] = {0};
     char dnssl[2560] = {0};
     char dnssl_lft[16];
@@ -929,6 +929,17 @@ static int gen_zebra_conf(int sefd, token_t setok)
 #endif
 
 #endif//_HUB4_PRODUCT_REQ_
+            if (i == 0) {
+               syscfg_get(NULL, "ra_lifetime", buf, sizeof(buf));
+            }
+            else {
+               sprintf(syscfgName, "ra_lifetime_%d", i+1);
+               syscfg_get(NULL, syscfgName, buf, sizeof(buf));
+            }
+            ra_lifetime = atoi(buf);
+            if (ra_lifetime <= 0)
+                ra_lifetime = 180;
+
             if(i == 0)
                strcpy(syscfgName, "ra_interval");
             else
@@ -946,7 +957,8 @@ static int gen_zebra_conf(int sefd, token_t setok)
             syscfg_get(NULL, syscfgName, buf, sizeof(buf));
             ra_interval = atoi(buf);
             if (ra_interval <= 0)
-                ra_interval = 3;
+                ra_interval = 180;
+            ra_interval = (ra_interval > ra_lifetime) ? ra_lifetime : ra_interval;
             fprintf(fp, "   ipv6 nd ra-interval %d\n", ra_interval);
 #else
             ra_interval = 180;
@@ -958,15 +970,6 @@ static int gen_zebra_conf(int sefd, token_t setok)
 #ifdef WAN_FAILOVER_SUPPORTED
             if (strcmp(default_wan_interface, wan_interface) != 0)
             {
-                int ra_lifetime;
-                if(i == 0)
-                   strcpy(syscfgName, "ra_lifetime");
-                else
-                   sprintf(syscfgName, "ra_lifetime_%d", i+1);
-                syscfg_get(NULL, syscfgName, buf, sizeof(buf));
-                ra_lifetime = atoi(buf);
-                if (ra_lifetime <= 0)
-                    ra_lifetime = 180;
                 fprintf(fp, "   ipv6 nd ra-lifetime %d\n", ra_lifetime);
             }
             else
@@ -980,15 +983,6 @@ static int gen_zebra_conf(int sefd, token_t setok)
                 }
                 else
                 {
-                    int ra_lifetime;
-                    if(i == 0)
-                       strcpy(syscfgName, "ra_lifetime");
-                    else
-                       sprintf(syscfgName, "ra_lifetime_%d", i+1);
-                    syscfg_get(NULL, syscfgName, buf, sizeof(buf));
-                    ra_lifetime = atoi(buf);
-                    if (ra_lifetime <= 0)
-                        ra_lifetime = 180;
                     fprintf(fp, "   ipv6 nd ra-lifetime %d\n", ra_lifetime);
                 }
             }
