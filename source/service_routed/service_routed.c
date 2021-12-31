74d24f856f34b4dabefe7b73c679c98347df5bc9
Tarun Saha <tsaha@libertyglobal.com>
2021-12-31 08:17:11 -0800

    fix default value of Router Advertisement Interval should be 180 ( OFW-2248 )
    Using default value of 180 for advertisement interval.

--- source/service_routed/service_routed.c
+++ source/service_routed/service_routed.c
@@ -576,7 +576,7 @@ static int gen_zebra_conf(int sefd, token_t setok)
     }
     FILE *fp = NULL;
     char rtmod[16], ra_en[16], dh6s_en[16];
-    int ra_interval;
+    int ra_interval, ra_lifetime;
     char name_servs[1024] = {0};
     char dnssl[2560] = {0};
     char dnssl_lft[16];
@@ -846,6 +846,15 @@ static int gen_zebra_conf(int sefd, token_t setok)
 
 #endif//_HUB4_PRODUCT_REQ_
             if(i == 0)
+               strcpy(syscfgName, "ra_lifetime");
+            else
+               sprintf(syscfgName, "ra_lifetime_%d", i+1);
+            syscfg_get(NULL, syscfgName, buf, sizeof(buf));
+            ra_lifetime = atoi(buf);
+            if (ra_lifetime <= 0)
+                ra_lifetime = 180;
+
+            if(i == 0)
                strcpy(syscfgName, "ra_interval");
             else
                sprintf(syscfgName, "ra_interval_%d",i+1);
@@ -862,7 +871,8 @@ static int gen_zebra_conf(int sefd, token_t setok)
             syscfg_get(NULL, syscfgName, buf, sizeof(buf));
             ra_interval = atoi(buf);
             if (ra_interval <= 0)
-                ra_interval = 3;
+                ra_interval = 180;
+            ra_interval = (ra_interval > ra_lifetime) ? ra_lifetime : ra_interval;
             fprintf(fp, "   ipv6 nd ra-interval %d\n", ra_interval);
 #else
             ra_interval = 180;
@@ -878,15 +888,6 @@ static int gen_zebra_conf(int sefd, token_t setok)
             }
             else
             {
-                int ra_lifetime;
-                if(i == 0)
-                   strcpy(syscfgName, "ra_lifetime");
-                else
-                   sprintf(syscfgName, "ra_lifetime_%d", i+1);
-                syscfg_get(NULL, syscfgName, buf, sizeof(buf));
-                ra_lifetime = atoi(buf);
-                if (ra_lifetime <= 0)
-                    ra_lifetime = 180;
                 fprintf(fp, "   ipv6 nd ra-lifetime %d\n", ra_lifetime);
             }
 #else
