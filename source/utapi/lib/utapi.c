ced4e7f52c6337df1d221546641a355566f80c5f
sreesreekumar <ssreekumar.contractor@libertyglobal.com>
2022-01-06 13:45:18 +0530

    fix lease time is ignored when port forwarding is setup via UPnP ( OFW-1848 CH7465OFW-515 )
    [Issue]
    Lease time is ignored when port forwarding is setup via UPnP
    
    [Solution]
    Code added to flush the connection.
    
    [Testing Performed]
    1.Added a portforward rule with lease time via upnp.
    2.Verified the communication via nc command.
    3.Verified that rule is deleted after lease time and no packet transfer via nc command.
    
    Signed-off-by: Sreeja Sreekumar (ssreekumar.contractor@libertyglobal.com)

--- source/utapi/lib/utapi.c
+++ source/utapi/lib/utapi.c
@@ -2905,6 +2905,7 @@ int Utopia_DeleteDynPortMappingIndex (int index)
 		if( UT_SUCCESS == rc_del ) 
 		{
 			Utopia_IPRule_ephemeral_port_forwarding( &pmap, FALSE );
+			v_secure_system("conntrack_flush &");
 		}
 #endif /* 0 */
     }
