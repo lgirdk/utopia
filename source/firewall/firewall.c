07e15e7c455aef390df6fa8fa9974573b22bfb35
Sarveswaran Nagarajan <sanagarajan.contractor@libertyglobal.com>
2022-01-19 14:02:12 +0530

    fix issues with IPv4 IP and Port filtering at selected times ( OFW-2044 F3896SI-549 )
    Using putenv() added in to TZ env
    
    Testing Performance: Scheduled Macfiltering and checked the functionality works
    
    Signed-off-by: Sarveswaran Nagarajan <sanagarajan.contractor@libertyglobal.com>

--- source/firewall/firewall.c
+++ source/firewall/firewall.c
@@ -16474,6 +16474,8 @@ int main(int argc, char **argv)
 {
    int rc = 0;
    char syslog_status[32];
+   char value[256];
+   char str[256];
    pid_t process_id;
    
    process_id = getpid();
@@ -16507,6 +16509,12 @@ int main(int argc, char **argv)
       }
    }
 //	pthread_mutex_init(&firewall_check, NULL);
+
+  syscfg_get(NULL, "TZ", value, sizeof(value));
+  if (value[0] != '\0'){
+    sprintf(str, "TZ=%s", value);
+    putenv(str);
+  }
 
 
   fw_shm_mutex fwmutex = fw_shm_mutex_init(SHM_MUTEX);
