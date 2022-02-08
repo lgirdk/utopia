395a2fa9f6df1b170d45b23d85b6e1303c6d8ccc
Devin Mungan <dmungan@libertyglobal.com>
2022-02-08 18:25:10 +0100

    firewall: fix slow tftp transfer from console on Puma6 ARM ( OFW-2354 CH7465SI-92 )
    [Issue]
    tftp client is very slow on the LAN network.
    The issue is caused by the anti-DOS filtering rules. Rate limiting
    applies to (most of) the incoming udp packets.
    
    [Solution]
    Using conntrack module, mark the connections initiated by the GW
    (mangle table -> OUTPUT chain) with the port 69 which is the port
    used by tftp protocol. Packets belonging to these marked connections
    shall then be allowed to pass through the network without limitation.
    
    [Testing Performed]
    - Initiate tftp put from within the GW console to a LAN client behind the GW.
    - Verify that the transfer is complete within reasonable time
      (a 500 KB file should be transferred in a matter of seconds)
    
    Signed-off-by: Devin Mungan(dmungan@libertyglobal.com)

--- source/firewall/firewall.c
+++ source/firewall/firewall.c
@@ -12341,6 +12341,9 @@ static int prepare_subtables(FILE *raw_fp, FILE *mangle_fp, FILE *nat_fp, FILE *
    fprintf(mangle_fp, "-A PREROUTING -i %s -p udp -m conntrack --ctstate NEW -m limit --limit 200/sec --limit-burst 100 -j ACCEPT\n",ecm_wan_ifname);
    fprintf(mangle_fp, "-A PREROUTING -i %s -p udp -m conntrack --ctstate NEW -m limit --limit 200/sec --limit-burst 100 -j ACCEPT\n",emta_wan_ifname);
 #endif
+#ifdef _PUMA6_ARM_
+   fprintf(mangle_fp, "-A OUTPUT -p udp --dport 69  -m conntrack --ctstate NEW -j CONNMARK --set-mark 0x1000/0x1000\n");
+#endif
    /*
     * nat
     */
@@ -14022,6 +14025,9 @@ static int prepare_disabled_ipv4_firewall(FILE *raw_fp, FILE *mangle_fp, FILE *n
    fprintf(mangle_fp, "-A PREROUTING -i %s -p udp -m conntrack --ctstate NEW -m limit --limit 200/sec --limit-burst 100 -j ACCEPT\n",ecm_wan_ifname);
    fprintf(mangle_fp, "-A PREROUTING -i %s -p udp -m conntrack --ctstate NEW -m limit --limit 200/sec --limit-burst 100 -j ACCEPT\n",emta_wan_ifname);
 #endif
+#ifdef _PUMA6_ARM_
+   fprintf(mangle_fp, "-A OUTPUT -p udp --dport 69  -m conntrack --ctstate NEW -j CONNMARK --set-mark 0x1000/0x1000\n");
+#endif
 
    /*
     * nat
@@ -14135,6 +14141,7 @@ static int prepare_disabled_ipv4_firewall(FILE *raw_fp, FILE *mangle_fp, FILE *n
       fprintf(filter_fp, "-A lan2self_dos_tcp -m recent --set --name lan2self_dos_tcp\n");
       fprintf(filter_fp, "-A lan2self_dos_tcp -m recent --update --seconds 1 --hitcount 15 --name lan2self_dos_tcp -j DROP\n");
 
+      fprintf(filter_fp, "-A lan2self_dos_udp -m conntrack --ctstate RELATED,ESTABLISHED -m connmark --mark 0x1000/0x1000 -j RETURN\n");
       fprintf(filter_fp, "-A lan2self_dos_udp -m recent --set --name lan2self_dos_udp\n");
       fprintf(filter_fp, "-A lan2self_dos_udp -m recent --update --seconds 1 --hitcount 15 --name lan2self_dos_udp -j DROP\n");
 
@@ -16616,6 +16623,7 @@ static int do_ipflooddetectv4(FILE *fp)
         fprintf(fp, "-A DOS_UDP -j DOS_DROP\n");
 
 #ifdef _PUMA6_ARM_
+        fprintf(fp, "-A lan2self_dos_udp -m conntrack --ctstate RELATED,ESTABLISHED -m connmark --mark 0x1000/0x1000 -j RETURN\n");
         fprintf(fp, "-A lan2self_dos_udp -m recent --set --name lan2self_dos_udp\n");
         fprintf(fp, "-A lan2self_dos_udp -m recent --update --seconds 1 --hitcount 15 --name lan2self_dos_udp -j DROP\n");
 #endif
@@ -16821,6 +16829,7 @@ static int do_ipflooddetectv6(FILE *fp)
         fprintf(fp, "-A DOS_UDP -j DOS_DROP\n");
 
 #ifdef _PUMA6_ARM_
+        fprintf(fp, "-A lan2self_dos_udp -m conntrack --ctstate RELATED,ESTABLISHED -m connmark --mark 0x1000/0x1000 -j RETURN\n");
         fprintf(fp, "-A lan2self_dos_udp -m recent --set --name lan2self_dos_udp\n");
         fprintf(fp, "-A lan2self_dos_udp -m recent --update --seconds 1 --hitcount 15 --name lan2self_dos_udp -j DROP\n");
 #endif
