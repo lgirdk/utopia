775769f7f6e57747d8b0351c08aa80f82833cee5
Andre McCurdy <armccurdy@gmail.com>
2022-06-09 19:49:51 -0700

    service_ntpd.sh: ignore new_ntp_enabled syscfg value (assume always true)
    The legacy behaviour (and PartnerID dependency) is not used for OFW.
    
    Upstream Candidate: No. LGI specific NTP requirement. MVXREQ-367
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/scripts/init/service.d/service_ntpd.sh
+++ source/scripts/init/service.d/service_ntpd.sh
@@ -241,7 +241,8 @@ service_start ()
    echo "restrict 127.0.0.1" >> $NTP_CONF_TMP
    echo "restrict -6 ::1" >> $NTP_CONF_TMP
 
-   if [ "$SYSCFG_new_ntp_enabled" = "true" ]; then
+   # OFW: Assume syscfg new_ntp_enabled" is always true
+
        if [ "$BOX_TYPE" = "SR300" ]; then
             IPV4_CONN_STATE=$(sysevent get ipv4_connection_state)
             if [ "$IPV4_CONN_STATE" != "up" ]; then
@@ -328,41 +329,6 @@ service_start ()
            echo_t "SERVICE_NTPD : NTP SERVERS(1-5 and from DHCPv4/6) are not available, not starting ntpd" >> $NTPD_LOG_NAME
            return 0
        fi
-   else
-
-       PARTNER_ID=`syscfg get PartnerID`
-
-       if [ -z "$SYSCFG_ntp_server1" ] || [ "$SYSCFG_ntp_server1" = "no_ntp_address" ]; then
-           if [ -z "$PARTNER_ID" ]; then
-               echo_t "SERVICE_NTPD : NTP SERVER 1 not available & PARTNER_ID is null, using the default ntp server." >> $NTPD_LOG_NAME
-               SYSCFG_ntp_server1="time1.google.com"
-           else
-               if [ -f "/nvram/ETHWAN_ENABLE" ]; then
-                  echo_t "SERVICE_NTPD : NTP SERVER 1 not available, using the default ntp server." >> $NTPD_LOG_NAME
-                  SYSCFG_ntp_server1="time1.google.com"
-               else
-                  echo_t "SERVICE_NTPD : NTP SERVER 1 not available, not starting ntpd" >> $NTPD_LOG_NAME
-                  return 0
-               fi
-           fi
-       fi
-
-        if [ "$BOX_TYPE" = "SR300" ]; then
-            IPV4_CONN_STATE=$(sysevent get ipv4_connection_state)           
-            if [ "$IPV4_CONN_STATE" != "up" ]; then
-                SYSCFG_ntp_server1="time.google.com"
-                SYSCFG_ntp_server2="time-e-g.nist.gov"
-                VALID_SEVER="true"
-            fi
-        fi
-
-       # Start NTP Config Creation with Legacy Single Server Setup
-       echo_t "SERVICE_NTPD : Creating NTP config" >> $NTPD_LOG_NAME
-
-       echo "server $SYSCFG_ntp_server1 true" >> $NTP_CONF_TMP
-       echo "restrict $SYSCFG_ntp_server1 nomodify notrap noquery" >> $NTP_CONF_TMP
-
-   fi # if [ "$SYSCFG_new_ntp_enabled" = "true" ]; then
 
    # Continue with Rest of NTP Config Creation
    if [ -z "$SOURCE_PING_INTF" ]; then
@@ -549,7 +515,7 @@ service_stop ()
 
 service_init ()
 {
-    queries="ntp_server1 ntp_server2 ntp_server3 ntp_server4 ntp_server5 ntp_enabled new_ntp_enabled"
+    queries="ntp_server1 ntp_server2 ntp_server3 ntp_server4 ntp_server5 ntp_enabled"
     get_utctx_val "$queries"
 }
 
