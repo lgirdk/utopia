cc44be6134d5131479fd13c38501c4e8548b7e4e
Mohamed Shaikh <mohamed.shaikh@t-systems.com>
2022-12-15 17:48:19 +0530

    RDKCOM-3504: SKYH4-6593 RDKBDEV-703 lan2wan_misc rule corrections
    Reason for change: VPN NAT Traversal
    VPN Passthrough: lan2wan_misc rule corrections
    1. The device must provide IPSec for VPN passthrough
    2. The device must provide L2TP for VPN passthrough
    3. The device must provide TLS/SSL for VPN passthrough
    4. The device must provide VPN pass-through for IPv6 - IPsec , TLS/SSL
    Test Procedure: Updated in JIRA
    Risks: None
    Signed-off-by: Mohamed Shaikh <mohamed.shaikh@t-systems.com>
    
    (cherry picked from commit 0381e009efd2303c6f8ef024ebaae4bf22b7fe95)
    Change-Id: I2c8b0ef438215a837bd1f19c7dd61f931457309f

--- source/firewall/firewall.c
+++ source/firewall/firewall.c
@@ -9679,19 +9679,29 @@ static int do_lan2wan_misc(FILE *filter_fp)
     {
         if((0==syscfg_get(NULL, "blockipsec::result", query, sizeof(query))) && strcmp(query,"DROP") == 0)
             fprintf(filter_fp, "-A lan2wan_misc -p udp --dport 500  -j DROP\n");
+        else if(strcmp(query,"ACCEPT") == 0)
+                fprintf(filter_fp, "-A lan2wan_misc -p udp --dport 500  -j ACCEPT\n");
         query[0] = '\0';
 
         if((0==syscfg_get(NULL, "blockl2tp::result", query, sizeof(query))) && strcmp(query,"DROP") == 0)
             fprintf(filter_fp, "-A lan2wan_misc -p udp --dport 1701  -j DROP\n");
+        else if(strcmp(query,"ACCEPT") == 0)
+                fprintf(filter_fp, "-A lan2wan_misc -p udp --dport 1701  -j ACCEPT\n");
         query[0] = '\0';
 
         if((0==syscfg_get(NULL, "blockpptp::result", query, sizeof(query))) && strcmp(query,"DROP") == 0)
             fprintf(filter_fp, "-A lan2wan_misc -p tcp --dport 1723  -j DROP\n");
+        else if(strcmp(query,"ACCEPT") == 0)
+                fprintf(filter_fp, "-A lan2wan_misc -p tcp --dport 1723  -j ACCEPT\n");
         query[0] = '\0';
 
         if((0==syscfg_get(NULL, "blockssl::result", query, sizeof(query))) && strcmp(query,"DROP") == 0){
             fprintf(filter_fp, "-A lan2wan_misc -p udp --dport 443  -j DROP\n");
             fprintf(filter_fp, "-A lan2wan_misc -p tcp --dport 443  -j DROP\n");
+        }
+        else if(strcmp(query,"ACCEPT") == 0){
+                fprintf(filter_fp, "-A lan2wan_misc -p udp --dport 443  -j ACCEPT\n");
+                fprintf(filter_fp, "-A lan2wan_misc -p tcp --dport 443  -j ACCEPT\n");
         }
     }
 #endif
@@ -15352,7 +15362,7 @@ v6GPFirewallRuleNext:
         fprintf(fp, "-A lan2wan_misc_ipv6 -p udp --dport 500  -j DROP\n");
         fprintf(fp, "-A lan2wan_misc_ipv6 -p udp --dport 4500  -j DROP\n");
     }
-    else {
+    else if(strcmp(queryv6,"ACCEPT") == 0){
         fprintf(fp, "-A lan2wan_misc_ipv6 -p udp --dport 500  -j ACCEPT\n");
         fprintf(fp, "-A lan2wan_misc_ipv6 -p udp --dport 4500  -j ACCEPT\n");
     }
@@ -15362,7 +15372,7 @@ v6GPFirewallRuleNext:
         fprintf(fp, "-A lan2wan_misc_ipv6 -p udp --dport 443  -j DROP\n");
         fprintf(fp, "-A lan2wan_misc_ipv6 -p tcp --dport 443  -j DROP\n");
     }
-    else {
+    else if(strcmp(queryv6,"ACCEPT") == 0){
         fprintf(fp, "-A lan2wan_misc_ipv6 -p udp --dport 443  -j ACCEPT\n");
         fprintf(fp, "-A lan2wan_misc_ipv6 -p tcp --dport 443  -j ACCEPT\n");
     }
@@ -15371,7 +15381,7 @@ v6GPFirewallRuleNext:
     if((0 == syscfg_get(NULL, "blockl2tp::result", queryv6, sizeof(queryv6))) && strcmp(queryv6,"DROP") == 0){
         fprintf(fp, "-A lan2wan_misc_ipv6 -p udp --dport 1701  -j DROP\n");
     }
-    else {
+    else if(strcmp(queryv6,"ACCEPT") == 0){
         fprintf(fp, "-A lan2wan_misc_ipv6 -p udp --dport 1701  -j ACCEPT\n");
     }
     queryv6[0] = '\0';
@@ -15379,7 +15389,7 @@ v6GPFirewallRuleNext:
     if((0 == syscfg_get(NULL, "blockpptp::result", queryv6, sizeof(queryv6))) && strcmp(queryv6,"DROP") == 0){
         fprintf(fp, "-A lan2wan_misc_ipv6 -p tcp --dport 1723  -j DROP\n");
     }
-    else {
+    else if(strcmp(queryv6,"ACCEPT") == 0){
         fprintf(fp, "-A lan2wan_misc_ipv6 -p tcp --dport 1723  -j ACCEPT\n");
     }
     fprintf(fp, "-I lan2wan -j lan2wan_misc_ipv6\n");
