5f1a4be1a1d273df5e7e03173015d2f8b4d95fac
mkamat <mkamat@libertyglobal.com>
2023-01-26 13:56:45 -0800

    [Mv2+] F3896LG-1641- Pentesting: improvements available for firewall config

--- source/firewall/firewall.c
+++ source/firewall/firewall.c
@@ -13615,7 +13615,14 @@ static int prepare_subtables(FILE *raw_fp, FILE *mangle_fp, FILE *nat_fp, FILE *
    fprintf(filter_fp, "-A lan2self -j lan2self_dos\n");
 #endif
    fprintf(filter_fp, "-A lan2self -j lan2self_plugins\n");
-   fprintf(filter_fp, "-A lan2self -m state --state NEW -j ACCEPT\n");
+
+   //As per MVXREQ-1032, No unused or unneeded service ports MUST be open on the device. The following list are the expected open ports in IPv4 RG mode.
+   //TCP: 53, 80, 1900, 5000, 8443	UDP:53, 67, 1900
+   //TBD In addition to above list,ports for known services 21(ftp),22(ssh),23(Telnet),554(rtsp),69(tftp),5060(SIP),5061(SIP), are added.This has to be revisited and these  ports will have to be eventually open only when respective service is enabled
+   fprintf(filter_fp, "-A lan2self -m state --state NEW -p tcp --match multiport --dports 53,80,1900,5000,8443,21,22,23,554,5060 -j ACCEPT\n");//tcp 53, 80, 1900, 5000, 8443,21(ftp),22(SSH),23(Telnet),554(rtsp)
+   fprintf(filter_fp, "-A lan2self -m state --state NEW -p udp --match multiport --dports 53,67,1900,69,5061 -j ACCEPT\n");//udp 53, 67, 1900, 69(tftp)
+   fprintf(filter_fp, "-A lan2self -m state --state NEW -j DROP\n");
+
    fprintf(filter_fp, "-A lan2self -m state --state RELATED,ESTABLISHED -j ACCEPT\n");
    fprintf(filter_fp, "-A lan2self -j ACCEPT\n");
 
