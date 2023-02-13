d50978954915108e0a96f025c861ca39d4020280
Suresh Putta <suputta@libertyglobal.com>
2023-02-13 10:30:07 -0800

    fix router lifetime to be set zero on wan reset ( OFW-4130 )

--- source/service_wan/service_wan.c
+++ source/service_wan/service_wan.c
@@ -1125,6 +1125,8 @@ static int wan_start(struct serv_wan *sw)
                sysevent_set(sw->sefd, sw->setok, "wan_service-status", "error", 0);
                return -1;
        }
+        //restart zebra service
+        sysevent_set(sw->sefd, sw->setok, "zebra-restart", NULL, 0);
 
     }
 
@@ -1343,6 +1345,8 @@ static int wan_stop(struct serv_wan *sw)
                sysevent_set(sw->sefd, sw->setok, "wan_service-status", "error", 0);
                return -1;
         }
+        //restart zebra service
+        sysevent_set(sw->sefd, sw->setok, "zebra-restart", NULL, 0);
     }
     
 
