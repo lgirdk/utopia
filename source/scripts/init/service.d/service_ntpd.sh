8eafbdd1ac9feaa52da9b0394c34bd229f3941e6
Maxim Sharkanov <maxim_sharkanov2@comcast.com>
2023-12-01 22:27:10 +0000

    LTE-2215 RDKB-52844: IDM restart happens before NTP sync in XLE
    Reason for change: ntpq tries to use IPv6 on IPv4 only
                       and fails. See ticket for more details.
    
    Test Procedure: ntp_time_sync should not misfire
    
    Risks: low
    Priority: p1
    Change-Id: I69b110d6eb82b55aa9534ee2fad892112a6df7d8
    Signed-off-by: Maxim_Sharkanov2@comcast.com
    (cherry picked from commit 0ecf3b6587f1e4c97eddc3bcf8bf4ae5be02658d)

--- source/scripts/init/service.d/service_ntpd.sh
+++ source/scripts/init/service.d/service_ntpd.sh
@@ -302,8 +302,13 @@ set_ntp_driftsync_status ()
       retry=1
       while true
       do
-        sync_status=`ntpq -c rv | grep "stratum=16"`
-        if [ -z "$sync_status" ]; then
+        if [ $WAN_IPv6_UP -eq 1 ]; then
+           sync_status=`ntpq -c rv | grep "stratum"`
+        else
+           sync_status=`ntpq -4 -c rv | grep "stratum"`
+        fi
+
+        if [ -n "$sync_status" ] && [[ "$sync_status" != *"stratum=16"* ]]; then
            echo_t "SERVICE_NTPD : ntpd time synced , setting the status" >> $NTPD_LOG_NAME
            syscfg set ntp_status 3
            sysevent set ntp_time_sync 1
