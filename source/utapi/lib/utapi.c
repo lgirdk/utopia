a6840b0993fcbf99db9c4ab1bba3b867e5fc4acb
James Cooley <james.cooley@rdkcentral.com>
2024-08-29 09:52:33 +0100

    Revert "RDKBDEV-2778: make _check_port_range() local."
    This reverts commit da55c24aa24c2a623d44ed4c0d83a41b067936af.
    
    Reason for revert: merged without rebasing
    
    Change-Id: Ib13861647366f0bf705374f455c6447ae60fd5a6

--- source/utapi/lib/utapi.c
+++ source/utapi/lib/utapi.c
@@ -3775,7 +3775,7 @@ int Utopia_DelPortTriggerByRuleId (UtopiaContext *ctx, int rule_id)
 
 #define PORT_OVER_RANGE(n_s, n_e, s, e) ((((n_s) > (e)) || ((n_e) < (s))) ? 0 : 1)
 
-static int _check_port_range( UtopiaContext *ctx, int new_rule_id, int new_start, int new_end, int new_protocol, UtopiaValue utopia[5], int is_trigger)
+int _check_port_range( UtopiaContext *ctx, int new_rule_id, int new_start, int new_end, int new_protocol, UtopiaValue utopia[5], int is_trigger)
 {
     char tokenbuf[256];
     int count;
@@ -3831,7 +3831,7 @@ static int _check_port_range( UtopiaContext *ctx, int new_rule_id, int new_start
     return 0;
 }
 
-static int _check_single_port_range( UtopiaContext *ctx, int new_rule_id, int new_start, int new_end, int new_protocol, UtopiaValue utopia[5], int is_trigger)
+int _check_single_port_range( UtopiaContext *ctx, int new_rule_id, int new_start, int new_end, int new_protocol, UtopiaValue utopia[5], int is_trigger)
 {
     char tokenbuf[256];
     int count;
